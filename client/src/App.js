1ab55a06fe2e9383c140b8a658e4aab69e3c7b4e
Adesegun Adedeji <adesegunadedeji@gmail.com>
2020-07-22 21:12:08 -0700

    Need to add additional styling

---
+++ client/src/App.js
@@ -0,0 +1,67 @@
+import React, {Component} from 'react';
+import {Grid} from '@material-ui/core';
+import youtube from './API/youtube';
+import {SearchBar, VideoList, VideoDetail} from './components';
+class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        }
+    }
+
+    componentDidMount(){
+        this.handleSubmit('Arsenal');
+    }
+
+    onVideoSelect = (video)=> {
+        this.setState({
+            selectedVideo: video
+        })
+
+    }
+    
+   handleSubmit = async(searchTerm)=> {
+       //console.log(`${process.env.REACT_APP_KEY}`, "API KEY")
+    const response = await youtube.get('search',
+
+    { params: {
+            part: 'snippet',
+            maxResults: 10,
+            key: `${process.env.REACT_APP_KEY}`,
+            q: searchTerm,
+        }
+    });
+    this.setState({
+        videos: response.data.items,
+        selectedVideo: response.data.items[0]
+    });
+    }
+
+render() {
+    const {selectedVideo, videos} = this.state;
+    return(
+        <Grid justify="center" container spacing={10}>
+            <Grid item xs={12}>
+                <Grid container spacing={10}>
+                <Grid item xs={12}>
+                    <SearchBar onFormSubmit = {this.handleSubmit}/>
+                    </Grid>
+                    <Grid item xs={8}>
+                        <VideoDetail video={selectedVideo}/>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <VideoList videos={videos} onVideoSelect={this.onVideoSelect}/>
+                    </Grid>
+
+                </Grid>
+
+            </Grid>
+        </Grid>
+    )
+}
+}
+export default App;
+
+
